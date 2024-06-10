773114622f9cc6be33d1171cbb2c65de063419d6
Rahul Sharma <rahulgaur116@gmail.com>
2024-06-10 15:37:34 -0400

    added util folfer and auth function

--- controllers/api/postroutes.js
+++ controllers/api/postroutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment  } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
 // create a new post ('/api/post')
 router.post('/', async (req, res) => {
--- controllers/dashroutes.js
+++ controllers/dashroutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 const sequelize = require('../config/config');
 
 // User's all posts ('/dashboard')
