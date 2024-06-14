c6e0a5e7b20257ec5a1a75cb2cd5cd2e5c7a5ae9
Rahul Sharma <rahulgaur116@gmail.com>
2024-06-14 13:55:10 -0400

    corrected edit post rputes

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,4 +1,6 @@
-const postId = document.querySelector('input[name="post-id"]').value;
+// const postId = document.querySelector('input[name="post-id"]').value;
+const editform = document.querySelector('#edit-form');
+
 
 const commentFormHandler = async (event) => {
     event.preventDefault();
@@ -24,6 +26,34 @@ const commentFormHandler = async (event) => {
     };
 } 
 
+
+const editformhandler = async (event) =>{
+    event.preventDefault();
+    const posttitle = document.querySelector('#post-title').value.trim();
+    console.log(posttitle);
+    const postcontent = document.querySelector('#post-content').value.trim();
+    console.log(postcontent);
+    const response = await fetch('/api/post/', {
+        method: 'PUT',
+        body: JSON.stringify({
+            comment: comment,
+            postId: postId,
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+
+}
 if(document.querySelector('.comment-form') !=null) {  
+
+
     document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
 }
+
+editform.addEventListener('submit',editformhandler);
